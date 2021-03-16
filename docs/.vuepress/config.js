7ba450261313590188974f9275e3e67e8d775b4a
liangjia <liangjia@variflight.com>
2021-03-16 18:38:17 +0800

    添加：

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    base:'/donivinck/',
+    title: 'Docker Web服务搭建',
+    description: '本文介绍 Docker 的基础知识，Dockerfile 的编写，Web 应用常见服务的搭建，包括 Redis 集群搭建，Mysql 集群搭建，以及使用 docker-compose 搭建适用于 Laravel 项目的 PHP Web 服务的项目实战',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Github', link: 'https://github.com/nivin-studio/donivinck' },
+        ]
+    }
+}
